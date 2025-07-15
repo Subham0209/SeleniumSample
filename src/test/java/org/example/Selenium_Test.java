527822c0867eb2e4b9c8ab552f1cd0263858506c
Shubham Das <shubham.das@wdc.com>
2025-07-15 18:41:12 +0530

    Update: Integrated BrowserStack for remote WebDriver execution

--- src/test/java/org/example/Selenium_Test.java
+++ src/test/java/org/example/Selenium_Test.java
@@ -11,20 +11,37 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
 
 public class Selenium_Test {
 
-     WebDriver driver;
+    public static final String USERNAME = "gopalbist_fs3NtA";
+    public static final String AUTOMATE_KEY = "q5uWYnH4psXJsnoxkcz1";
+
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+    WebDriver driver;
 
      @BeforeClass
-     public void setup() {
-     WebDriverManager.chromedriver().setup();
-     driver = new ChromeDriver();
+     public void setup() throws Exception{
+         DesiredCapabilities caps = new DesiredCapabilities();
+         caps.setCapability("os", "Windows");
+         caps.setCapability("os_version", "10");
+         caps.setCapability("browser", "Chrome");
+         caps.setCapability("browser_version", "latest");
+         caps.setCapability("name", "Google Search Test on BrowserStack");
+
+         driver = new RemoteWebDriver(new URL(URL), caps);
  }
 
-     @Test
-     public void googleSearchTest() {
-     driver.get("https://www.westerndigital.com");
+    @Test
+    public void googleSearch() {
+        driver.get("https://www.google.com");
+        System.out.println("Title: " + driver.getTitle());
+    }
 
      /*WebElement searchBox = driver.findElement(By.name("q"));
      searchBox.sendKeys("TestNG with Selenium");
@@ -40,13 +57,13 @@ public class Selenium_Test {
      /*String title = driver.getTitle();
      System.out.println("Page Title: " + title);
      assert title.toLowerCase().contains("testng with selenium");*/
+
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null)
+            driver.quit();
+    }
  }
 
-     @AfterClass
-     public void tearDown() {
-     if (driver != null) {
-         driver.quit();
-     }
- }
-}
 
