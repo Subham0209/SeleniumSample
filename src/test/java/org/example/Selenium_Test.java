5e9a1ed7f1117ec459d1bd5e7c7acfff86593470
Shubham Das <shubham.das@wdc.com>
2025-07-15 22:21:10 +0530

    Fix: Updated BrowserStack WebDriver setup with env variables_1

--- src/test/java/org/example/Selenium_Test.java
+++ src/test/java/org/example/Selenium_Test.java
@@ -15,41 +15,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.sql.Driver;
+import java.util.HashMap;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Selenium_Test {
 
-    public static final String USERNAME = "gopalbist_fs3NtA";
-    public static final String AUTOMATE_KEY = "q5uWYnH4psXJsnoxkcz1";
+        WebDriver driver ;
 
-    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+        public void setup() throws Exception {
+            // BrowserStack options
+            HashMap<String, Object> browserstackOptions = new HashMap<>();
+            browserstackOptions.put("os", "Windows");
+            browserstackOptions.put("osVersion", "10");
+            browserstackOptions.put("buildName", "Selenium_BS_Build");
+            browserstackOptions.put("sessionName", "Google Search Test on BrowserStack");
+            browserstackOptions.put("debug", true);
+            browserstackOptions.put("networkLogs", true);
 
-    WebDriver driver;
+            // Main capabilities
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("browserName", "Chrome");
+            caps.setCapability("browserVersion", "latest");
+            caps.setCapability("bstack:options", browserstackOptions);
 
-     @BeforeClass
-     public void setup() throws MalformedURLException {
-         String username = System.getenv("BROWSERSTACK_USERNAME");
-         String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-
-         if (username == null || accessKey == null) {
-             throw new IllegalArgumentException("BrowserStack credentials not set in environment variables.");
-         }
-
-         DesiredCapabilities caps = new DesiredCapabilities();
-         caps.setCapability("browserName", "Chrome");
-         caps.setCapability("browserVersion", "latest");
-         caps.setCapability("os", "Windows");
-         caps.setCapability("osVersion", "10");
-         caps.setCapability("name", "Google Search Test on BrowserStack");
-         caps.setCapability("build", "Selenium_BS_Build");
-         caps.setCapability("browserstack.debug", "true");
-         caps.setCapability("browserstack.networkLogs", "true");
-
-         System.out.println("Connecting to BrowserStack with URL...");
-         String url = "https://" + username + ":" + accessKey + "@hub-cloud.browserstack.com/wd/hub";
-         driver = new RemoteWebDriver(new URL(url), caps);
-     }
+            // Start remote session
+            driver = new RemoteWebDriver(
+                    new URL("https://USERNAME:ACCESS_KEY@hub-cloud.browserstack.com/wd/hub"),
+                    caps
+            );
+        }
 
 
     @Test
